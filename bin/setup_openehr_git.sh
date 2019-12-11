e792f037ca7877614da52b9dce65a40f84f3a125
wolandscat <thomas.beale@openehr.org>
2019-12-11 18:05:20 +0000

    Add script-copying.

--- bin/setup_openehr_git.sh
+++ bin/setup_openehr_git.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # script to clone all specifications related repos from openEHR Github area
+# and set up some useful scripts for the user
 
+# -------------- GitHub clones --------------
 # context = orgs | users
 context="orgs"
-
-# name = openEHR
 name="openEHR"
 
 # repo keys
@@ -23,6 +23,14 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 			echo "git clone $repo_clone_url"
 		fi
 	done
+fi
+
+# -------------- Copy scripts --------------
+control_repo="specifications-AA_GLOBAL"
+if [ -d $control_repo ]; then
+	cp $control_repo/bin/do_*.sh .
+else
+	echo "Control repo $control_repo not cloned"
 fi
 
 exit 0
